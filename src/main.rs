3aaf6f1d27ea4712b59226de84bd16e6ed671fc5
2547techno <technowizz2547@gmail.com>
2024-07-28 01:10:47 -0400

    bump version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::{arg, command, Arg, ArgAction, Command};
 mod commands;
 mod utils;
 
-const VERSION_STR: &str = "v0.1a";
+const VERSION_STR: &str = "v0.2a";
 
 fn main() {
     let matches = command!()
